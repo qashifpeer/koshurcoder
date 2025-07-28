edeb6bd82b221e1622081b3b157a16aa9c0b3e31
qashifpeer <mail.qashifpeer@gmail.com>
2025-07-28 19:30:32 +0530

    bootstrap code

---
+++ src/constants/feedback.ts
@@ -0,0 +1,46 @@
+export interface FeedbackCardProps {
+    name: string;
+    address: string;
+    review: string;
+    image: string;
+  }
+  
+  export const feedbackData: FeedbackCardProps[] = [
+    {
+      name: "Abishek Kumar",
+      address: "Ranchi, India",
+      review: "An amazing experience! The team organized everything perfectly.",
+      image: "/images/feedbackPeople/dummy_m.jpg",
+    },
+    {
+      name: "Manoj Sinha",
+      address: "Jaipur,India",
+      review: "Beautiful locations and well-organized trips. Highly recommended!",
+      image: "/images/feedbackPeople/dummy_m.jpg",
+    },
+    {
+      name: "Saransh Vishvekarma",
+      address: "Nagpur, India",
+      review: "A wonderful adventure! Thanks to the team for a memorable trip.",
+      image: "/images/feedbackPeople/dummy_m.jpg",
+    },
+    {
+      name: "Abishek Kumar",
+      address: "Ranchi, India",
+      review: "An amazing experience! The team organized everything perfectly.",
+      image: "/images/feedbackPeople/dummy_m.jpg",
+    },
+    {
+      name: "Abishek Kumar",
+      address: "Ranchi, India",
+      review: "Beautiful locations and well-organized trips. Highly recommended!",
+      image: "/images/feedbackPeople/dummy_m.jpg",
+    },
+    {
+      name: "Saransh Vishvekarma",
+      address: "Nagpur, India",
+      review: "A wonderful adventure! Thanks to the team for a memorable trip.",
+      image: "/images/feedbackPeople/dummy_m.jpg",
+    },
+  ];
+  
